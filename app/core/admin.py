685fdcc6e7b39621ac92b8c157b34757be1eb77e
santosh <santoshg.seo@gmail.com>
2021-09-08 19:48:03 +0530

    Added working list tags endpoints

--- app/core/admin.py
+++ app/core/admin.py
@@ -25,4 +25,6 @@ class UserAdmin(BaseUserAdmin):
             'fields' : ('email', 'password1', 'password2')
         }),
     )
+
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
---
+++ app/recipe/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from core.models import Tag
+
+
+class TagSeralizer(serializers.ModelSerializer):
+    """Serializer for tags objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
